3617e0a8efed706db727b8ae5a5f09920612f232
Lev Lionn <levster81@gmail.com>
2019-07-25 13:50:07 -0400

    init commit
    My side project for camera integration and learnign more about databases.

---
+++ data-selfie/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const Datastore = require("nedb");
+const app = express();
+const fs = require("fs");
+
+app.listen(3000, () => console.log("Listening at 3000"));
+app.use(express.static(__dirname + "/public/"));
+
+app.use(express.json({ limit: "1mb" }));
+
+const db = new Datastore("clientLocations.db");
+db.loadDatabase();
+
+let requestCount = 0;
+app.post("/api", (request, response) => {
+  requestCount++;
+
+  if (requestCount > 1) {
+    console.log("Got another request!");
+  } else {
+    console.log("First request!");
+  }
+  const data = JSON.stringify(request.body);
+  const timeStamp = Date();
+  data.timeStamp = timeStamp;
+  var imgData = data.image64;
+
+  fs.writeFileSync("synchronous.txt", data);
+  // db.insert(data);
+  response.json(data);
+});
+
+app.get("/api", (request, response) => {
+  db.find({}, (err, data) => {
+    response.json(data);
+  });
+});
